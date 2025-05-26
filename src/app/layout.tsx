fa965980453b2750cb47911e17951284a586f92e
lwnl <liangwang.fr@gmail.com>
2025-05-26 16:51:35 +0200

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-row`}
+        >
+          <div className="w-1/5 h-screen bg-gray-50">
+            <Navbar />
+          </div>
+          <div className="w-4/5 h-screen">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
