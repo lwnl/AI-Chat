ce871be51b141f77eb1b9e5c37ec8f8ead7fcd8e
lwnl <liangwang.fr@gmail.com>
2025-06-08 13:41:54 +0200

    update

--- src/components/MobileMenuButton.tsx
+++ src/components/MobileMenuButton.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useMenu } from "@/context/MenuContext";
-import { Menu } from "lucide-react";
+import { Icon, Menu, X } from "lucide-react";
 
 export default function MobileMenuButton() {
-  const { setMenuOpen } = useMenu();
+  const { menuOpen, setMenuOpen } = useMenu();
+  const Icon = menuOpen ? X : Menu;
+
   return (
-    <Menu
-      className="fixed top-10 left-6 z-50 block sm:hidden"
+    <Icon
+      className="fixed top-10 left-6 z-50 block cursor-pointer sm:hidden"
       onClick={() => {
         setMenuOpen((prev) => !prev);
       }}
